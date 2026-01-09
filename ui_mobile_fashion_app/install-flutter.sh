8a112d0782f5e95f96bb105a9bc3c42f04eccefc
TienDatttt-dev <nguyentiendat686821@gmail.com>
2026-01-09 21:16:32 +0700

    okkkkk

--- ui_mobile_fashion_app/install-flutter.sh
+++ ui_mobile_fashion_app/install-flutter.sh
@@ -4,12 +4,12 @@
 # 1. Cấu hình PATH
 export PATH="$PATH:$HOME/flutter/bin"
 
-# 2. Cài đặt Flutter nếu chưa có (Dùng cache Netlify)
+# 2. Cài đặt Flutter nếu chưa có
 if [ ! -d "$HOME/flutter" ]; then
     git clone https://github.com/flutter/flutter.git -b stable $HOME/flutter
 fi
 
-# 3. TẠO FILE TRONG THƯ MỤC ASSETS (Bắt buộc để build web không lỗi 404)
+# 3. Tạo file env trong assets
 mkdir -p assets
 echo "API_BASE_URL=$API_BASE_URL" > assets/.env.dev
 echo "API_VERSION=$API_VERSION" >> assets/.env.dev
@@ -18,6 +18,5 @@ echo "APP_NAME=$APP_NAME" >> assets/.env.dev
 # 4. Build
 flutter config --enable-web
 flutter pub get
-# Bỏ renderer gây lỗi, để Flutter tự quyết định
-flutter build web --release -t lib/main_admin.dart --dart-define=FLAVOR=admin
-  --dart-define=FLAVOR=admin
+# Viết tất cả trên cùng 1 dòng để tránh lỗi ngắt dòng của Linux
+flutter build web --release -t lib/main_admin.dart --dart-define=FLAVOR=admin --no-wasm-dry-run
